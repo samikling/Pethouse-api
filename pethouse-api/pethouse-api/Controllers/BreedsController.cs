f3a12598e47ace2086423300835f8729b2adb8a1
sami kling <sakling@live.com>
2021-07-16 13:43:28 +0300

    PET-15 Added breedsController

---
+++ pethouse-api/pethouse-api/Controllers/BreedsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pethouse_api.Models;
+
+namespace pethouse_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreedsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{key}")]
+        //Hae kaikki lemmikit
+        public Breeds GetAllPets(int key)
+        {
+            pethouseContext db = new pethouseContext();
+            Breeds breed = db.Find<Breeds>(key);
+            return breed;
+        }
+    }
+}
